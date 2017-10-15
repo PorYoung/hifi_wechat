07a5386da9cbea3ea0c2feb613de85ab42eb3f0e
PorYoung <poryoung@foxmail.com>
2017-10-15 21:29:42 +0800

    change message and subtitle to default  show

--- server/controller/permission/index.js
+++ server/controller/permission/index.js
@@ -3,7 +3,11 @@ import url from 'url'
 export default class {
   static loginRequire(req, res, next){
     const openid = url.parse(req.query.origin || '', true).query.openid
-    if (!req.session.openid || !openid || req.session.openid !== openid) {
+
+    //后期需要对不同用户进行鉴权
+    if(req.session.username && req.query.origin){
+      console.log(req.session.username)
+    }else if (!req.session.openid || !openid || req.session.openid !== openid) {
       return res.sendStatus(403)
     }
     next()
