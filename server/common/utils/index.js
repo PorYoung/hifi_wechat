806038c9d98b8ba6461c98406ac97d2411c7b875
shuang6 <319825685@qq.com>
2017-10-27 03:45:35 +0800

    Remove Vue-cli

--- server/common/utils/index.js
+++ server/common/utils/index.js
@@ -1,12 +1,12 @@
 import fs from 'fs'
 import path from 'path'
 import https from 'https'
-import {URL} from 'url'
+import { URL } from 'url'
 
 const readFileSync = dir => {
-    return new Promise((resolve,reject) => {
-        fs.readFile(path.join(__dirname,dir),(err,data) => {
-            if(err) reject(err)
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, dir), (err, data) => {
+            if (err) reject(err)
             else resolve(data)
         })
     })
@@ -28,7 +28,7 @@ const httpsGetJSON = url => {
         })
     })
 }
-const httpsPostJSON = (url,info) => {
+const httpsPostJSON = (url, info) => {
     url = new URL(url)
     let postData = info
     let options = {
@@ -41,8 +41,8 @@ const httpsPostJSON = (url,info) => {
             'Content-Length': postData.length
         }
     }
-    return new Promise((resolve,reject) => {
-        let req = https.request(options,res => {
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, res => {
             res.on('data', data => {
                 let result
                 try {
@@ -62,31 +62,31 @@ const httpsPostJSON = (url,info) => {
         req.end();
     })
 }
-const httpsGetFile = (url,filename) => {
-    return new Promise((resolve,reject) => {
-            let wOption = {
-                flags: 'a',
-                encoding: null,
-                mode: 0o666
+const httpsGetFile = (url, filename) => {
+    return new Promise((resolve, reject) => {
+        let wOption = {
+            flags: 'a',
+            encoding: null,
+            mode: 0o666
+        }
+        let fileWriteStream = fs.createWriteStream(path.join(__dirname, '/../../../static/' + filename), wOption);
+        https.get(url, res => {
+            try {
+                let status = JSON.parse(res.headers)
+                if (!!status.connection && status.connection == "close") reject(err)
+            } catch (err) {
+                //需要优化
             }
-            let fileWriteStream = fs.createWriteStream(path.join(__dirname,'/../../../static/'+filename),wOption);
-            https.get(url,res => {
-                try{
-                    let status = JSON.parse(res.headers)
-                    if(!!status.connection&&status.connection=="close") reject(err)
-                }catch(err){
-                    //需要优化
-                }
-                res.on('data',data=>{
-                    fileWriteStream.write(data)
-                })
-                res.on('end',()=>{
-                  fileWriteStream.end()
-                  let src = '/static' + filename
-                  resolve(src)
-                })
-                res.on('err',err=>reject(err))
+            res.on('data', data => {
+                fileWriteStream.write(data)
             })
+            res.on('end', () => {
+                fileWriteStream.end()
+                let src = '/static' + filename
+                resolve(src)
+            })
+            res.on('err', err => reject(err))
+        })
     })
 }
 
